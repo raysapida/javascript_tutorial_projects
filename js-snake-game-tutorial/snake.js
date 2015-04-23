b0c26699636c530c1dfc0f28df70aa29de84a412
Raymond Sapida <ray.sapida@gmail.com>
2015-04-22 18:12:32 -0700

    Create new function that moves the snake object whenever it's called

--- js-snake-game-tutorial/snake.js
+++ js-snake-game-tutorial/snake.js
@@ -3,5 +3,13 @@ var drawSnake = function(snakeToDraw) {
   var drawableObjects = [drawableSnake];
   CHUNK.draw(drawableObjects);
 }
+
+var moveSnake = function(snake) {
+  var oldSegment = snake[0];
+  var newSegment = { top: oldSegment.top + 1, left: oldSegment.left };
+  var newSnake = [newSegment];
+  return newSnake;
+}
+
 var snake = [{ top: 0, left: 0}];
 drawSnake(snake);
