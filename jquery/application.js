059d29eceb91fefc3fd00479b962541f48db54b0
Raymond Sapida <ray.sapida@gmail.com>
2015-04-25 15:22:50 -0700

    Extract adding list to its own line and change h2 background color

--- jquery/application.js
+++ jquery/application.js
@@ -3,5 +3,7 @@ $(function (){
   $("div.article").append(backToTop);
   var toc = "<h2 id='toc_header'>Table of Contents</h2>";
   var list = "<ul id='toc_list'></ul>";
-  $("h1").after(toc).after(list);
+  $("h1").after(toc);
+  $("h2:first").after(list);
+  $("div.article h2").css("background-color","#CCF")
 });
