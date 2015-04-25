d165be460b3fc0f77a1eb89fb573fabc13caa1f9
Raymond Sapida <ray.sapida@gmail.com>
2015-04-25 15:17:49 -0700

    Create h2 header for ToC and a ul tag after that

--- jquery/application.js
+++ jquery/application.js
@@ -1,4 +1,7 @@
 $(function (){
   var backToTop = "<a href='#top'>Back to Top</a>";
   $("div.article").append(backToTop);
+  var toc = "<h2 id='toc_header'>Table of Contents</h2>";
+  var list = "<ul id='toc_list'></ul>";
+  $("h1").after(toc).after(list);
 });
