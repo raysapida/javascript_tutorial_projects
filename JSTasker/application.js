4410fb47684f93e1ab80d573cb038f6c4315281c
Raymond Sapida <ray.sapida@gmail.com>
2015-04-25 18:10:49 -0700

    Remove functions from global namespace to a JSTasker namespace and change call to updateTaskCounter to this new variable

--- JSTasker/application.js
+++ JSTasker/application.js
@@ -1,14 +1,3 @@
-function updateTaskCounter(){
-  var task_count = $('div#tasks ul').children().not('li.completed').size();
-  $('span#task_counter').text(task_count);
-  sortTasks();
-}
-function sortTasks(){
-  var taskList = $('div#tasks ul');
-  var allCompleted = taskList.children('.completed');
-  allCompleted.detach();
-  taskList.append(allCompleted)
-}
 $(function(){
   $('input#task_text').focus();
   $('form#add_task').on('submit', function(event){
@@ -19,8 +8,24 @@ $(function(){
     $('input#task_text').val('');
     $('ul li').on('click', function(event){
       $(this).toggleClass('completed');
-      updateTaskCounter();
+      JSTasker.updatePage();
     });
-    updateTaskCounter();
+    JSTasker.updatePage();
   });
 });
+var JSTasker = {
+  updateTaskCounter: function() {
+    var taskCount = $('div#tasks ul').children().not('li.completed').size();
+    $('span#task_counter').text(task_count);
+  },
+  sortTasks: function() {
+    var taskList = $('div#tasks ul');
+    var allCompleted = taskList.children('.completed');
+    allCompleted.detach();
+    taskList.append(allCompleted)
+  },
+  updatePage: function (){
+    this.updateTaskCounter();
+    this.sortTasks();
+  }
+};
