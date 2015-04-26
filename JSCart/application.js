504788cfd99fc8a026a8d2edd769b21b2516e7a2
Raymond Sapida <ray.sapida@gmail.com>
2015-04-25 18:27:20 -0700

    Iterate over the inventory var and check that the name can be accessed

--- JSCart/application.js
+++ JSCart/application.js
@@ -2,4 +2,7 @@ $(function(){
   var inventory = $(rawInventory);
   var prototypeItem = $('#prototype-item');
   prototypeItem.detach();
+  inventory.each(function(){
+    alert("Inserting " + this.name);
+  });
 });
