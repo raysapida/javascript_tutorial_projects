09a9e67fd3b310440de5f1d6d304f6daa9cd45c3
Raymond Sapida <ray.sapida@gmail.com>
2015-04-26 17:42:44 -0700

    Add more tests but TypeError results

--- jasmine/spec/testing_intro/CircleSpec.js
+++ jasmine/spec/testing_intro/CircleSpec.js
@@ -1,8 +1,17 @@
 describe("Circle", function() {
   var Circle = require('../../lib/testing_intro/Circle');
+  var circle;
+
+  beforeEach(function() {
+    circle = new Circle(47, 32, 10);
+  });
 
   it("should have an x coordinate", function() {
-    var circle = new Circle(47);
     expect(circle.x).toEqual(47);
+  });
+
+  it("should draw on the canvas", function() {
+    var context = hmmmm; // ???
+    circle.draw(context);
   });
 });
