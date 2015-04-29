3a28b13a97aef0cad957d85a370a393daa4f1e4b
Raymond Sapida <ray.sapida@gmail.com>
2015-04-28 23:47:34 -0700

    First assignment from planetproto on object literals

--- nodeschool/planetproto/010.js
+++ nodeschool/planetproto/010.js
@@ -1,9 +1,11 @@
 // -> Create an object called 'robot' using an object literal
 // -> robot should have a property 'smart' with value true
-__
+var robot = {
+  smart: true
+};
 
 // -> Claim the result robot.smart
-claim(robot.smart, __);
+claim(robot.smart, true);
 
 // ------------------------------------------------
 // Common JS exports for verification, don't modify
