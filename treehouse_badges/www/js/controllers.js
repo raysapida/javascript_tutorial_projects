07f77dc923fb574d08cd073d29b199186be00eca
Raymond Sapida <ray.sapida@gmail.com>
2015-06-12 21:15:40 -0700

    Remove default ionic template markup and add call to treehouse api to get user information

--- treehouse_badges/www/js/controllers.js
+++ treehouse_badges/www/js/controllers.js
@@ -1,28 +1,15 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('DashCtrl', function($scope, $http) {
+  //Get profile information
+  $http.get('https://teamtreehouse.com/raymondsapida.json').
+  success(function(data, status, headers, config) {
+    // this callback will be called asynchronously
+    // when the response is available
+    $scope.badges = data.badges;
+  }).
+    error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
 });
