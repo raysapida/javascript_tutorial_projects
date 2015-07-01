d7800628ed1be8de60bcb6d24b70f9e8e9f7239e
Raymond Sapida <ray.sapida@gmail.com>
2015-07-01 13:15:57 -0700

    Exercise on using call and hasOwnProperty in JS

---
+++ nodeschool/functional-javascript-workshop/08-call.js
@@ -0,0 +1,7 @@
+function duckCount() {
+  return Array.prototype.slice.call(arguments).filter(function(object) {
+    return Object.prototype.hasOwnProperty.call(object, 'quack')
+  }).length
+}
+
+module.exports = duckCount
