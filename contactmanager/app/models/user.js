c5cbabf3da12d87a95395fa2c1f6db0e874cd3d7
Raymond Sapida <ray.sapida@gmail.com>
2015-08-28 03:53:58 -0700

    Set up app adapter to use fixture data
    > ember generate adapter application

--- contactmanager/app/models/user.js
+++ contactmanager/app/models/user.js
@@ -7,3 +7,14 @@ export default DS.Model.extend({
   postCode: DS.attr(),
   country: DS.attr()
 });
+
+User.reopenClass({
+  FIXTURES: [{
+    id: 1,
+    firstName: 'James',
+    lastName: 'Rice',
+    addressLine: '66 Belvue Road',
+    postCode: 'M235PS',
+    country: 'United Kingdom'
+  }]
+});
